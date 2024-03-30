b7ba26f2109d88b753ac81062d832df025b7d36b
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-03-30 15:20:33 +0530

    Writing Complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,12 @@
 function ExpenseItem(){
     return <>
-        <p>Food - $5</p>
-        <p>Petrol - $20</p>
-        <p>Movie - $10</p>
+    <div>15 August 2023</div>
+    <div>
+      <h2>Book</h2>
+      <div>$10</div>
+      <div>Delhi</div>
+
+    </div>
     </>
 }
 export default ExpenseItem;
