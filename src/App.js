a10c6c53bfc76bb13fd59d9418a6bfb2c1000b46
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-03-30 15:04:31 +0530

    First Custom Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App(){
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <p>I am learning React</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
+
 export default App;
