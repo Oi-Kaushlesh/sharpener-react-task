5a955711e3cf64fe3df9c258cdce524928dedb99
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-04-02 00:46:55 +0530

    NEXT ASSIGNMENT - Advance REACT - Listening for Events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,20 @@
-// Write your code at relevant places in the code below
+// Write your code at the relevant places in the code below
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from './Card'
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  function buttonClick(){
+    console.log("Button clicked")
+  }
   return (
     <Card className="expense-item">
-      <ExpenseDate date= {props.date} ></ExpenseDate>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonClick}>Change Title</button>
       </div>
     </Card>
   );
