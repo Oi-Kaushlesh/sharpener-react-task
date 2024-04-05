8a0cbd9ddd10786b269448cbcef2e0ca8317ef97
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-04-06 00:00:01 +0530

    Controlled Components - Expenses Filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,18 @@
-// Write your code at the relevant places in the code below
-import React,  {useState} from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-const ExpenseItem= (props)=> {
-  const [title, setTitle] = useState(props.title)
-  function buttonClick(){
-    setTitle("New Title");
-  }
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={buttonClick}>Change Title</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
